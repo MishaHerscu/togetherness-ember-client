aa7e374ee762d765f79441e46c5f8233c099af12
MishaHerscu <mishaherscu@gmail.com>
2016-07-29 15:40:49 -0400

    set up ember client

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'togetherness-ember-client',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
