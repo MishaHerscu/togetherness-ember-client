ddf97c3ce683058ccb684515921a6587507005e1
MishaHerscu <mishaherscu@gmail.com>
2016-08-01 15:32:43 -0400

    showing recommendations

--- app/attraction-suggestions/route.js
+++ app/attraction-suggestions/route.js
@@ -4,7 +4,43 @@ export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('attraction-suggestion')
     .then((result) => {
-      return result.toArray().slice(0,40);
+      return result.toArray().sort(() => {
+        return 0.5 - Math.random();
+      }).slice(0,20);
+    })
+    .then((result) => {
+      let attractionIds = [];
+      result.forEach((r) => {
+        attractionIds.push(String(r.get('attraction_id')));
+      });
+      return this.get('store').findAll('attraction')
+      .then((attractions) => {
+        let kept_attractions = attractions.toArray().filter((attraction) => {
+          return attractionIds.includes(String(attraction.get('id')));
+        });
+        return kept_attractions;
+      });
     });
+  },
+
+  actions: {
+    likeAttraction(attraction){
+      let user_attraction = {
+        user_id: Number(this.get('credentials').get('id')),
+        attraction_id: Number(attraction.id),
+        like: true
+      };
+      let attraction_rating = this.get('store').createRecord('user_attraction', user_attraction);
+      attraction_rating.save();
+    },
+    dislikeAttraction(attraction) {
+      let user_attraction = {
+        user_id: Number(this.get('credentials').get('id')),
+        attraction_id: Number(attraction.id),
+        like: false
+      };
+      let attraction_rating = this.get('store').createRecord('user_attraction', user_attraction);
+      attraction_rating.save();
+    },
   },
 });
