808320d582d5125867133a8c1c5f0f0b747a6535
MishaHerscu <mishaherscu@gmail.com>
2016-08-01 09:44:32 -0400

    passing action up successfully

--- app/components/attraction-event/component.js
+++ app/components/attraction-event/component.js
@@ -2,15 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'div',
+  classNameBindings: 'hiddenAttraction:hidden',
+  hiddenAttraction: false,
 
   attractionEvents: {},
 
   actions: {
-    likeAttraction (attraction) {
-      this.sendAction('likeAttraction', attraction);
+    likeAttraction () {
+      this.sendAction('likeAttraction', this.get('attraction'));
     },
-    dislikeAttraction (attraction) {
-      this.sendAction('dislikeAttraction', attraction);
+    dislikeAttraction () {
+      this.sendAction('dislikeAttraction', this.get('attraction'));
+    },
+    closeAttraction () {
+      this.toggleProperty('hiddenAttraction');
     },
   },
 });
