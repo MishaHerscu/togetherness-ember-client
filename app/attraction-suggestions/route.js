c920ae2ec59e842bc25c9d2a9474d45b13de7b69
MishaHerscu <mishaherscu@gmail.com>
2017-03-03 19:30:45 -0600

    fixed up attraction-suggestions route

--- app/attraction-suggestions/route.js
+++ app/attraction-suggestions/route.js
@@ -1,19 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   model () {
+
+    let errorReturn = (error) => {
+      return Ember.RSVP.hash({
+        attractions: [],
+        recBool: false,
+        error: error
+      });
+    };
+
     return this.get('store').findAll('attraction-suggestion')
     .then((result) => {
-      return result.toArray().sort(() => {
-        return 0.5 - Math.random();
-      }).slice(0,20);
-    })
-    .then((result) => {
-      let attractions = [];
-      result.forEach((attraction) => {
-        attractions.push(this.get('store').findRecord('attraction', attraction.id));
-      });
-      return attractions;
+      let attractions = result.toArray();
+      let recBool = attractions.length > 0 ? true : false;
+      try {
+        return attractions.sort(() => {
+          return 0.5 - Math.random();
+        }).slice(0,20)
+        .then((result) => {
+          try {
+            let attractions = [];
+            result.forEach((attraction) => {
+              attractions.push(this.get('store').findRecord('attraction', attraction.id));
+            });
+            return Ember.RSVP.hash({
+              attractions: attractions,
+              recBool: recBool
+            });
+          } catch (error) {
+            return errorReturn(error);
+          }
+        });
+      } catch (error) {
+        return errorReturn(error);
+      }
     });
   },
 
